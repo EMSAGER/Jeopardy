ffe0c2fbe1cb6569e313656c323fde5431d4e92d
Emily <emsager7@gmail.com>
2023-07-06 08:12:43 -0700

    japi working again

--- jeopardy.js
+++ jeopardy.js
@@ -19,64 +19,6 @@
 //  ]
 
 
-const testClues = 
-[
-       { title: "Math",
-         clues: [
-           {question: "1+1", answer: 2, showing: null},
-           {question: "1+2", answer: 3, showing: null},
-           {question: "2+2", answer: 4, showing: null},
-           {question: "2+3", answer: 5, showing: null},
-           {question: "3+3", answer: 6, showing: null},
-         ],
-       },
-       { title: "Literature",
-         clues: [
-           {question: "Hamlet Author", answer: "Shakespeare", showing: null},
-           {question: "Bell Jar Author", answer: "Plath", showing: null},
-           {question: "Dune Author", answer: "Herbert", showing: null},
-           {question: "American Gods", answer: "Gaiman", showing: null},
-           {question: "Hobbit", answer: "Tolkien", showing: null},
-         ],
-       },
-       { title: "Science",
-         clues: [
-           {question: "Star in our galaxy", answer: "sun", showing: null},
-           {question: "third planet", answer: "earth", showing: null},
-           {question: "3 phases of an element", answer: "solid, liquid, gas", showing: null},
-           {question: "first planet", answer: "Mercury", showing: null},
-           {question: "no longer a planet", answer: "Pluto", showing: null},
-         ],
-       },
-       { title: "History",
-         clues: [
-           {question: "american indpendence", answer: 1776, showing: null},
-           {question: "treaty of tripoli author", answer: "Adams", showing: null},
-           {question: "western colonization reasoning", answer: "manifest destiny", showing: null},
-           {question: "first democracy", answer: "Greece", showing: null},
-           {question: "only state that was independent", answer: "Texas", showing: null},
-         ],
-       },{ title: "Art",
-       clues: [
-        {question: "starry night", answer: "van gogh", showing: null},
-        {question: "sistine chapel", answer: "michelangelo", showing: null},
-        {question: "la jaconda", answer: "da Vinci", showing: null},
-        {question: "soup cans", answer: "warhol", showing: null},
-        {question: "rothko chapel", answer: "rothko", showing: null},
-      ],
-     },
-     { title: "Weather",
-       clues: [
-         {question: "atlantic nope", answer: "hurricane", showing: null},
-         {question: "northwest pacific nope", answer: "cyclone", showing: null},
-         {question: "land nope", answer: "tornado", showing: null},
-         {question: "sky nope", answer: "hail", showing: null},
-         {question: "tree nope", answer: "wind", showing: null},
-       ],
-     },
-       
-     ];
-
                     //Create constants based on your parameters. what are your parameters? Number of Categories = 6, number of Clues per Category = 5;
 const numOfCategories = 6;
 const questionsPerCategory = 5;
@@ -92,12 +34,12 @@ async function getRandomCategory(){
  * Returns array of category ids
  */
 
-    //let res = await axios.get('https://jservice.io/api/categories?count=100');
-            //console.log(res.data);
-    //let catIDs = res.data.map(cat => cat.id);
-            //console.log(catIDs);
-    //return _.sampleSize(catIDs, numOfCategories);
-    //console.log(randomCats);
+    let res = await axios.get('https://jservice.io/api/categories?count=100');
+            console.log(res.data);
+    let catIDs = res.data.map(cat => cat.id);
+            console.log(catIDs);
+    return _.sampleSize(catIDs, numOfCategories);
+    console.log(randomCats);
     
 }
 
@@ -115,22 +57,22 @@ async function getCategory(catID) {
  *   ]
  */
 
-    // let res = await axios.get(`https://jservice.io/api/category?id=${catID}`);
-    // let cat = res.data;
-    // console.log(cat);
-    // let allClues = cat.clues;
-    // console.log(allClues);
-    // let randomClues = _.sampleSize(allClues, questionsPerCategory);
-    // //console.log(randomClues);
-    // let clues = randomClues.map(clue =>({
-    //    question: clue.question,
-    //     answer: clue.answer,
-    //     showing: null,
-    //     }));
-    // //console.log(clues);
-    // //console.log( {title: cat.title, clues});
-    // return  {title: cat.title, clues};
-    return testClues;
+    let res = await axios.get(`https://jservice.io/api/category?id=${catID}`);
+    let cat = res.data;
+    //console.log(cat);
+    let allClues = cat.clues;
+    //console.log(allClues);
+    let randomClues = _.sampleSize(allClues, questionsPerCategory);
+    //console.log(randomClues);
+    let clues = randomClues.map(clue =>({
+       question: clue.question,
+        answer: clue.answer,
+        showing: null,
+        }));
+    //console.log(clues);
+    //console.log( {title: cat.title, clues});
+    return  {title: cat.title, clues};
+    
 }
 
 /** Fill the HTML table#jeopardy with the categories & cells for questions.
@@ -148,9 +90,9 @@ async function fillTable() {
 
     let $tr = $("<tr>");
     for(let catIdx = 0; catIdx < numOfCategories; catIdx++){
-        //console.log(testClues[catIdx].title);
-        $tr.append($("<th>")).text(testClues[catIdx].title);
-        //$tr.append($("<th>")).text(Categories[catIdx].title);
+        console.log(testClues[catIdx].title);
+       // $tr.append($("<th>")).text(testClues[catIdx].title);
+        $tr.append($("<th>")).text(Categories[catIdx].title);
     };
     //console.log($tr);
     $jeopardyHead.append($tr);
--- swjeopardy.js
+++ swjeopardy.js
@@ -76,25 +76,26 @@ async function getCategory(catID) {
         };
       }
     })
-    let cluesObject = {
-      title: catName,
-      clues: cluesArray};
+    // let cluesObject = {
+    //   title: catName,
+    //   clues: cluesArray};
     //console.log(cluesObject);
-    let allClues = cluesObject.clues;
+    let allClues = cluesArray;
     //console.log(allClues);
   
     // let allClues = cat.clues;
     // // console.log(allClues);
-    let randomClues = _.sampleSize(allClues, questionsPerCategory);
-    console.log(randomClues);
+    let clues = _.sampleSize(allClues, questionsPerCategory);
+    //console.log(randomClues);
     // let clues = randomClues.map(clue =>({
     //    question: clue.question,
     //     answer: clue.answer,
     //     showing: null,
     //     }));
-    // // //console.log(clues);
+    // console.log(clues);
     // // //console.log( {title: cat.title, clues});
-    // return  {title: cat.title, clues};
+    // console.log({title: catName, clues});
+    return  {title: catName, clues};
     
 }
 
@@ -107,25 +108,25 @@ async function getCategory(catID) {
  */
 
 async function fillTable() {
-    // //empty out the Jeopardy Head
-    // $jeopardyHead.empty();
-    // //add a row with headers with categories - now loop through the array of Objects and each index to get the title
+    //empty out the Jeopardy Head
+    $jeopardyHead.empty();
+    //add a row with headers with categories - now loop through the array of Objects and each index to get the title
 
-    // let $tr = $("<tr>");
-    // for(let catIdx = 0; catIdx < numOfCategories; catIdx++){
-    //     $tr.append($("<th>")).text(testClues[catIdx].title);
-    // };
-    // //console.log($tr);
-    // $jeopardyHead.append($tr);
+    let $tr = $("<tr>");
+    for(let catIdx = 0; catIdx < numOfCategories; catIdx++){
+        $tr.append($("<th>")).text(testClues[catIdx].title);
+    };
+    //console.log($tr);
+    $jeopardyHead.append($tr);
 
-    // //Add rows w/ questions & clear out the body
-    // //think of the connect 4 {x}-{y} coordinates
-    // $jeopardyBody.empty();
-    // for (let clueIdx = 0; clueIdx < questionsPerCategory; clueIdx++){
-    //     console.log($tr);
-    //     $tr.append($("<td>")).attr("id",`${catIdx}-${clueIdx}`);
-    //     console.log($tr);
-    // }
+    //Add rows w/ questions & clear out the body
+    //think of the connect 4 {x}-{y} coordinates
+    $jeopardyBody.empty();
+    for (let clueIdx = 0; clueIdx < questionsPerCategory; clueIdx++){
+        console.log($tr);
+        $tr.append($("<td>")).attr("id",`${catIdx}-${clueIdx}`);
+        console.log($tr);
+    }
 
 }
 
